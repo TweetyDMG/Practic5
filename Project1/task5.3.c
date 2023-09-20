c053f21edf88df799be1b12a7df4d10795b91018
Artem Rogachev <144269255+TweetyDMG@users.noreply.github.com>
2023-09-20 11:32:03 +0300

    Update task5.3.c

--- Project1/task5.3.c
+++ Project1/task5.3.c
@@ -1,14 +1,22 @@
+#define _CRT_SECURE_NO_DEPRECATE
 #include <stdio.h>
+#include <stdlib.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include <locale.h>
 
 int main()
 {
     setlocale(LC_CTYPE, "RUS");
+    
+    //РљРѕРЅС‚СЂРѕР»СЊРЅС‹Р№ РїСЂРёРјРµСЂ: С… = 2.7, y = 1.7576
+    double x;
+    printf("Р’РІРµРґРёС‚Рµ x: ");
+    scanf("%lf", &x);
+    double t = -6;
+    double a = log(x);
+    double b = sqrt(x * x + t * t);
+    double y = pow(fabs(a - b * x), 1. / 5);
 
-    printf("Привет, мир!\n");
-
-    int num;
-    printf("Введите число: ");
-    scanf("%d", &num);
-    printf("Вы ввели: %d\n", num);
+    printf("Р РµР·СѓР»СЊС‚Р°С‚: %.4lf", y);
 }
